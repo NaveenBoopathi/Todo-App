5e69b32a33b9e9b4c7d883e9370913b085d7688c
Naveen Boopathi <naveenboopathi18@outlook.com>
2018-05-24 12:12:09 +0530

    Implement Completed Functionality

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Todo-App
+
+To be short, this application is mainly used to make a list of our daily tasks.
+
+Steps to run this application:
+
+1.  Download it into your local desktop.
+2.  Double click on index.html to run this.
+
+Please drop a mail to naveenboopathi18@outlook.com for any clarifications.
--- main.js
+++ main.js
@@ -2,17 +2,40 @@ console.log('starting!');
 
 $(document).ready(function() {
   var newTodo;
-  var todoList = [
-    'Learn HTML 5, CSS 3 and JavaScript',
-    'Create an Todo App using HTML and jQuery'
-  ];
-  if (todoList.length > 0) {
-    for (i = 0; i < todoList.length; i++) {
-      newTodo =
-        '<li><label><input type="checkbox" /><span> ' +
-        todoList[i] +
-        '</span></label></li>';
+  var todoList = [];
+
+  generateTodoList();
+
+  function generateTodoList() {
+    if (todoList.length > 0) {
+      for (i = 0; i < todoList.length; i++) {
+        newTodo =
+          '<li><label><input type="checkbox" id="todo' +
+          i +
+          '" /><span> ' +
+          todoList[i] +
+          '</span></label></li>';
+        $('#todoList').append(newTodo);
+      }
+      bindCheckboxChange();
+    } else {
+      newTodo = '<span class="highlight">Lets add some task here!</span>';
       $('#todoList').append(newTodo);
+    }
+  }
+
+  function removeTodoList(checkboxId) {
+    if (checkboxId || checkboxId === 0) {
+      var element = $('ul li').eq(checkboxId);
+      $(element).fadeOut('slow', function() {
+        $('#todoList').empty();
+        console.log('inside if');
+        generateTodoList();
+      });
+    } else {
+      $('#todoList').empty();
+      console.log('inside else');
+      generateTodoList();
     }
   }
 
@@ -20,14 +43,29 @@ $(document).ready(function() {
     var todoItem = $('#todoItem').val();
     if (todoItem) {
       newTodo =
-        '<li><label><input type="checkbox" /><span> ' +
+        '<li><label><input type="checkbox" id="todo' +
+        todoList.length +
+        '"/><span> ' +
         todoItem +
         '</span></label></li>';
-      $('#todoList').append(newTodo);
       todoList.push(todoItem);
+      removeTodoList();
       $('#todoItem').val('');
     } else {
       alert('Please enter an item to add!');
     }
   });
+
+  function bindCheckboxChange() {
+    $('input[type = checkbox]').change(function() {
+      console.log('called!');
+      if ($(this).prop('checked')) {
+        var checkboxId = $(this).attr('id');
+        checkboxId = parseInt(checkboxId.slice(4), 10);
+        console.log(checkboxId);
+        todoList.splice(checkboxId, 1);
+        removeTodoList(checkboxId);
+      }
+    });
+  }
 });
